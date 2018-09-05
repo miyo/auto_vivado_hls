80c21badccb4a4944d347ed560e4aa52cfa39cf0
Takefumi MIYOSHI <miyo@wasamon.net>
2018-09-05 05:34:47 -0400

    change analysis order

--- auto_vivado_hls.sh
+++ auto_vivado_hls.sh
@@ -20,33 +20,41 @@ echo "output IP-core version" ${VERSION}
 
 function run_vivado_hls_with_period() {
 	suffix=$1
+	array=$2
 	for period in $(echo ${TARGET_PERIOD} | tr -s ':' ' ')
 	do
 		prefix=$(echo $period | tr -s '.' '_')
 		prj=${suffix}_${prefix}
-		echo $prj
 		echo "target period" ${period}
 		echo "PROJ=${prj} PERIOD=${period} vivado_hls -f run_hls.tcl"
 		PROJ=${prj} PERIOD=${period} vivado_hls -f run_hls.tcl
+		array+=(${prj}) 
 	done
 }
 
 function run_vivado_hls() {
+	array=$2
 	echo "running VivadoHLS in $1"
 	source $1/settings64.sh
-	tmp=( $(echo $i | tr -s '/' ' ') )
-	i=$(expr ${#tmp[@]} - 1)
-	suffix=$(echo ${tmp[${i}]} | tr -s '.' '_')
+	tmp=( $(echo $1 | tr -s '/' ' ') )
+	idx=$(expr ${#tmp[@]} - 1)
+	suffix=$(echo ${tmp[${idx}]} | tr -s '.' '_')
 	prj=${TARGET}_${suffix}
-	run_vivado_hls_with_period ${prj}
+	run_vivado_hls_with_period ${prj} ${array}
 }
 
+array=()
 for i in ${VIVADO}/*
 do
-	run_vivado_hls $i
+	run_vivado_hls ${i} ${array}
 done
 
 if type python > /dev/null 2>&1; then
-	python tools/analysis.py */solution1/syn/report/${TARGET}_csynth.xml 
+	rpt=()
+	for i in ${array[@]}
+	do
+		rpt+=($i/solution1/syn/report/${TARGET}_csynth.xml)
+	done
+	python tools/analysis.py ${rpt[@]}
 fi
 
